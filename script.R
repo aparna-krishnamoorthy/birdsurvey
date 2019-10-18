f5b5d166ba016adfb781dbe978113ce016873c89
Aparna <apk52@pitt.edu>
2019-10-18 15:23:26 -0400

    hello

--- script.R
+++ script.R
@@ -2,19 +2,6 @@
 
 Analyze bird data
 
-strsplit('Hello,world',',') 
-strsplit('Hello,,,world',',') 
+hello 
 
-# lists can contain lists
-a.list <- list(letters[1:3]) # contains "a", "b", "c"
-another.list <- list(1:5) # contains 1,2,3,4,5
-still.another.list <- list(TRUE,FALSE,TRUE)
-I.am.a.list <- list(a.list,another.list,still.another.list)
-
-I.am.a.list
-unlist(I.am.a.list)
-unlist(I.am.a.list, recursive = FALSE)
-
-
-rep(1:4, 3)
 
