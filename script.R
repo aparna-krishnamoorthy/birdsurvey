1a6f0fcfa1e4379e55f9b740652f03171020a9fa
Aparna <apk52@pitt.edu>
2019-10-18 14:34:31 -0400

    Add figures
    Add table

--- script.R
+++ script.R
@@ -1,4 +1,19 @@
 # script to anazlyse bird counts r
 
-read data file
-run analysis
+
+strsplit('Hello,world',',') 
+strsplit('Hello,,,world',',') 
+
+# lists can contain lists
+a.list <- list(letters[1:3]) # contains "a", "b", "c"
+another.list <- list(1:5) # contains 1,2,3,4,5
+still.another.list <- list(TRUE,FALSE,TRUE)
+I.am.a.list <- list(a.list,another.list,still.another.list)
+
+I.am.a.list
+unlist(I.am.a.list)
+unlist(I.am.a.list, recursive = FALSE)
+
+
+rep(1:4, 3)
+
